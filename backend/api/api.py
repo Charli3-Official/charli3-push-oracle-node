44902981f7824e07e7d32c3923b00fbe57e14d75
MartorSkull <martin_montane_6@hotmail.com>
2022-06-10 17:49:30 -0300

    Adapted the _request function to be async and other upgrades

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
-import requests
+from ast import parse
+import asyncio
+import aiohttp
+
+class ApiResponse(object):
+    """Proxy class for library responses. Used to make the _request method
+    agnostic to the library it uses."""
+    def __init__(self, resp):
+        self._resp = resp
+
+    async def get_info(self):
+        self.status = self._resp.status
+        self.json = await self._resp.json()
+        self.ok = self._resp.ok
+        self.headers = self._resp.headers
 
 class Api(object):
     """Abstract class to make an agnostic implementation of http requests"""
@@ -9,12 +23,14 @@ class Api(object):
         "Accepts": "application/json"
     }
 
-    def _request(self, method, path, data, headers=dict()):
+    async def _request(self, method, path, data, headers=dict()):
         headers = self._header.update(headers)
-        resp = requests.request(
-            method=method,
-            url=f"{self.api_url}{path}",
-            json=data,
-            headers=headers
-        )
-        return resp
+        async with aiohttp.ClientSession() as session:
+            async with session.request(
+                    method,
+                    f"{self.api_url}{path}",
+                    json=data,
+                    headers=headers) as resp:
+                pars = ApiResponse(resp)
+                await pars.get_info()
+                return pars
