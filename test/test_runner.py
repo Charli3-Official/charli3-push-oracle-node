56ce028f3955263df2abe59d9bca38445c884850
Ale Gadea <ale@imagine.ai>
2022-07-28 18:54:47 -0300

    Fixing and adding some tests.

--- test/test_runner.py
+++ test/test_runner.py
@@ -34,14 +34,12 @@ MOCK_ORACLE = Oracle(
     'de031116866f1688d288b8eb42d1c321c0a2ecaf4acb05bbf7757c02',
     ('716e6a0dc6ade9c74eae49bfb3f006e809a131d9e5f201f631f8b7d4', 'CHARLI3')
 )
-MOCK_NODEMOCK = NodeMock(
-    MOCK_ORACLE,
-    **{
-        'pkh': '3a1314fa60a312d41eaf203378a6a92b5fca5c6649580e0c3e4fa707',
-        'wallet_id': '71517afc9a4d6dd79294ff6be77dd6a6a3c70d95',
-        'api_url': 'http://localhost:9080/api'
-    }
-)
+MOCK_NODEMOCK = {
+    'pkh': '3a1314fa60a312d41eaf203378a6a92b5fca5c6649580e0c3e4fa707',
+    'wallet_id': '71517afc9a4d6dd79294ff6be77dd6a6a3c70d95',
+    'api_url': 'http://localhost:9080/api'
+}
+
 MOCK_ORACLE_SETTINGS = OracleSettings(**{
     'node_pkhs': [
         'node1',
@@ -49,7 +47,7 @@ MOCK_ORACLE_SETTINGS = OracleSettings(**{
         'node3',
         'node4'
     ],
-    'required_nodes': 3500,
+    'required_nodes': 6000,
     'node_expiry': 300000,
     'aggregate_time': 720000,
     'aggregate_change': 500,
@@ -66,49 +64,78 @@ MOCK_CHAIN_QUERY = chainQueryTypes['blockfrost'](**{
 })
 
 #@pytest.fixture
-MOCKED_RUNNER_CASES = {
-    'nodes_updated': 0,
-    'req_nodes': 0,
-    'new_rate': 465210,
-    'own_feed': Feed(
-        value=466087,
-        timestamp=1657297865999,
-        initialized=True
-    ),
-    'expected_results':{
-        'update_calls':1,
-        'aggregate_calls':0,
-        'update_aggregate_calls':0
+MOCKED_RUNNER_CASES = [
+    {
+        'nodes_updated': 0,
+        'req_nodes': 3,
+        'new_rate': 465210,
+        'own_feed': Feed(
+            value=466087,
+            timestamp=1657297865999,
+            initialized=True
+        ),
+        'oracle_feed': Feed(
+            value=466087,
+            timestamp=1657297865999,
+            initialized=True
+        ),
+        'expected_results':{
+            'update_calls':1,
+            'aggregate_calls':0,
+            'update_aggregate_calls':0
+        }
     },
-    'oracle_feed': Feed(
-        value=466087,
-        timestamp=1657297865999,
-        initialized=True
-    )
-}
+    {
+        'nodes_updated': 2,
+        'req_nodes': 3,
+        'new_rate': 465210,
+        'own_feed': Feed(
+            value=466087,
+            timestamp=1657297865999,
+            initialized=True
+        ),
+        'oracle_feed': Feed(
+            value=466087,
+            timestamp=1657297865999,
+            initialized=True
+        ),
+        'expected_results':{
+            'update_calls':0,
+            'aggregate_calls':0,
+            'update_aggregate_calls':1
+        }
+    }
+]
 
 @pytest.mark.asyncio
 class TestFeedUpdaterClass():
     """Class to Test Runner"""
 
-    async def test_feed_operate(self):
+    async def test_without_updates(self):
+        """Oracle without any updated nodes"""
+        await self._feed_operate(MOCKED_RUNNER_CASES[0])
+
+    async def test_update_aggregate(self):
+        await self._feed_operate(MOCKED_RUNNER_CASES[1])
+
+    async def _feed_operate(self, case):
         """Method to test update - aggregate cases"""
 
         feed_updater = FeedUpdater(
             MOCK_UPDATE_INTERVAL,
             MOCK_ORACLE_SETTINGS,
-            MOCK_NODEMOCK,
+            NodeMock(MOCK_ORACLE, **MOCK_NODEMOCK),
             MOCK_RATE_CLASS,
             MOCK_CHAIN_QUERY
             )
 
-        update_calls = MOCKED_RUNNER_CASES['expected_results']['update_calls']
-        aggregate_calls = MOCKED_RUNNER_CASES['expected_results']['aggregate_calls']
-        update_aggregate_calls = MOCKED_RUNNER_CASES['expected_results']['update_aggregate_calls']
+        update_calls = case['expected_results']['update_calls']
+        aggregate_calls = case['expected_results']['aggregate_calls']
+        update_aggregate_calls = case['expected_results']['update_aggregate_calls']
 
-        del MOCKED_RUNNER_CASES['expected_results']
+        del case['expected_results']
 
-        await feed_updater.feed_operate(**MOCKED_RUNNER_CASES) # pylint: disable = E1123
+        await feed_updater.feed_operate(**case) # pylint: disable = E1123
 
         assert feed_updater.node.update_calls == update_calls
         assert feed_updater.node.aggregate_calls == aggregate_calls
